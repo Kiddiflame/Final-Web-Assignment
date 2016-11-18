e405c0a0df73ee269a3393f43e712152809db111
Kiddiflame <kiddiflame@gmail.com>
2016-11-18 12:03:44 +0000

    add a self updating footer date

--- index.php
+++ index.php
@@ -21,6 +21,18 @@
 	</ul>
 </div>
 <div class="dropdown"></div>
-<footer></footer>
+<footer>
+	<p>&copy;
+<?php
+$startYear = 2006;
+$thisYear = date('Y');
+if ($startYear == $thisYear) {
+ echo $startYear;
+} else {
+ echo "{$startYear}&ndash;{$thisYear}";
+}
+?>
+David Powers</p>
+</footer>
 </body>
 </html>
