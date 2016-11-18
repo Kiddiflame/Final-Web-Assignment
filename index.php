f30021df721c14a8b42d8178ac685ba360198224
Kiddiflame <kiddiflame@gmail.com>
2016-11-18 12:09:13 +0000

    slims down the footer code for the date

--- index.php
+++ index.php
@@ -23,15 +23,7 @@
 <div class="dropdown"></div>
 <footer>
 	<p>&copy;
-<?php
-$startYear = 2006;
-$thisYear = date('Y');
-if ($startYear == $thisYear) {
- echo $startYear;
-} else {
- echo "{$startYear}&ndash;{$thisYear}";
-}
-?>
+<p>&copy; 2006&ndash;<?php echo date('Y'); ?> David Powers</p>
 David Powers</p>
 </footer>
 </body>
