02487ee297d13381dcda819fce46cbea17161a9f
Kiddiflame <kiddiflame@gmail.com>
2016-11-22 09:37:09 +0000

    lagaði position reglur á footer, bætti flex í search text

--- index.php
+++ index.php
@@ -57,12 +57,21 @@
 	</ul>
 </div>
 <div class="search_text">
-	<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
-	</p>
+	<div class="block"><p>Sed ut persp</p></div>
+  <div class="block"><p>blabla</p></div>
+   <div class="block"><p>blabla</p></div>
+    <div class="block"><p>blabla</p></div>
+
 </div>
 <footer>
 <p>&copy;<?php echo date('Y'); ?> Kristinn Logi</p>
-<p class="contact_info">Email: Kiddiflame@gmail.com<br>Phone: 1234567<br></p>
+ <div class="Contact_Information">
+                    <p>
+                        Kristinn Logi<br>
+                        kiddiflame@gmail.com<br>
+                        555-5555<br>
+                    </p>
+                </div>
 </footer>
 </body>
 </html>
