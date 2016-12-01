f7b0b96483969672fd2a39184964b89f19d3dbf7
Andromeda-Explorer <thomasarip@gmail.com>
2016-12-01 13:57:14 +0000

    Bæta við dálk, lagaði css

--- index.php
+++ index.php
@@ -139,13 +139,9 @@ selected_school">Necromancy</option>
 <option value="Transmutation
 selected_school">Transmutation</option>
 
-
-
 </select>
 
 </li>
-
-
     <form action="#" method="POST">
       <input type="submit" name="Search">
     </form>
@@ -207,6 +203,10 @@ file_get_contents('test.json');
 </section>
 
 </div>
+
+<div class="info"><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></div>
+
+
 <footer>
  <div class="Contact_Information">
                     <p>
